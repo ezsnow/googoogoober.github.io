8b36fee4ad79f0530617ed18d7f9b4923cfad7de
sirbobisherenow <chetan.likes.nature@gmail.com>
2025-12-19 21:08:27 -0800

    ifix

--- games/balatro/game.js
+++ games/balatro/game.js
@@ -20,8 +20,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'game.data?v=d7e34743-2fea-4de6-8a0e-1103b5fcf07f';
-    var REMOTE_PACKAGE_BASE = 'game.data?v=d7e34743-2fea-4de6-8a0e-1103b5fcf07f';
+    var PACKAGE_NAME = 'game.data';
+    var REMOTE_PACKAGE_BASE = 'game.data';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       Module.printErr('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
